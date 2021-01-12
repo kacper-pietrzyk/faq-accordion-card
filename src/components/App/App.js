d909035b46b10ab84195ac00b2bbb34b1786f9fc
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-12 20:17:52 +0100

    initial structure

---
+++ src/components/App/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+import image from '../../assets/images/illustration-woman-online-mobile.svg';
+import shadowOfImage from '../../assets/images/bg-pattern-mobile.svg';
+
+const App = () => {
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.images}>
+          <img className={styles.images__mainImage} src={image} alt="online technologies" />
+          <img className={styles.images__shadowImage} src={shadowOfImage} alt="shadow" />
+        </div>
+        <h1 className={styles.main__headline}>FAQ</h1>
+        <ul className={styles.main__list}>
+          <li className={styles.main__listItem}>
+            <details>
+              <summary>How many team members can I invite?</summary>
+              <p>As much as you want</p>
+            </details>
+          </li>
+          <li className={styles.main__listItem}>
+            <details>
+              <summary>What is the maximum file upload size?</summary>
+              <p>No more than 2GB. All files in your account must fit your allotted storage space</p>
+            </details>
+          </li>
+          <li className={styles.main__listItem}>
+            <details>
+              <summary>How many team members can I invite?</summary>
+              <p>As much as you want</p>
+            </details>
+          </li>
+          <li className={styles.main__listItem}>
+            <details>
+              <summary>What is the maximum file upload size?</summary>
+              <p>No more than 2GB. All files in your account must fit your allotted storage space</p>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+};
+
+export default App;
