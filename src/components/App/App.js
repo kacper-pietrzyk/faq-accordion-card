384f5207834606202bb35159fea4b972bd8c396b
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-16 14:35:40 +0100

    move List to new component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './App.module.scss';
 
+import List from '../List/List';
+
 import image from '../../assets/images/illustration-woman-online-mobile.svg';
 import shadowOfImage from '../../assets/images/bg-pattern-mobile.svg';
 
@@ -14,32 +16,7 @@ const App = () => {
           <img className={styles.images__shadowImage} src={shadowOfImage} alt="shadow" />
         </div>
         <h1 className={styles.headline}>FAQ</h1>
-        <ul className={styles.list}>
-          <li className={styles.listItem}>
-            <details>
-              <summary>How many team members can I invite?</summary>
-              <p>As much as you want</p>
-            </details>
-          </li>
-          <li className={styles.listItem}>
-            <details>
-              <summary>What is the maximum file upload size?</summary>
-              <p>No more than 2GB. All files in your account must fit your allotted storage space</p>
-            </details>
-          </li>
-          <li className={styles.listItem}>
-            <details>
-              <summary>How many team members can I invite?</summary>
-              <p>As much as you want</p>
-            </details>
-          </li>
-          <li className={styles.listItem}>
-            <details>
-              <summary>What is the maximum file upload size?</summary>
-              <p>No more than 2GB. All files in your account must fit your allotted storage space</p>
-            </details>
-          </li>
-        </ul>
+        <List />
       </div>
     </>
   )
