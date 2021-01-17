bd9148cd37014ab926932031fe4dfa52c97f7ed1
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-17 20:42:20 +0100

    add arrow image

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './ListItem.module.scss';
 
+import arrow from '../../assets/images/icon-arrow-down.svg';
+
 const ListItem = () => {
   return (
     <li className={styles.listItem}>
-      <details>
-        <summary className={styles.listItem__question}>How many members can I invite?</summary>
+      <details className={styles.listItem__details}>
+        <summary className={styles.listItem__question}>How many members can I invite?
+        <div className={styles.listItem__arrow}><img src={arrow} alt="arrow" /></div>
+        </summary>
         <p className={styles.listItem__answer}>As much as you want</p>
       </details>
     </li>
