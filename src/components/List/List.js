5551762df6879b123fd48cf0f7d32c399aafe29a
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-19 21:17:21 +0100

    func: fetch data

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './List.module.scss';
 
 import ListItem from '../ListItem/ListItem';
 
 const List = () => {
+
+  const [content, setContent] = useState([]);
+
+  const functionToSetContent = () => {
+    const url = "http://www.json-generator.com/api/json/get/bVpOOFOEky?indent=2";
+    fetch(url)
+      .then(response => response.json())
+      .then(result => setContent(result.faq))
+      .catch(err => console.log(`błąd: ${err}`))
+  }
+
+  const data = content.map(data => (
+    <ListItem
+      key={data.id}
+      question={data.question}
+      answer={data.answer}
+    />
+  ))
+
   return (
-    <ul className={styles.list}>
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
+    <ul className={styles.list} onMouseDown={functionToSetContent}>
+      {data}
+      <li>coś</li>
     </ul>
   )
 };
