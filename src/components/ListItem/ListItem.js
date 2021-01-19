fa36da4d3843a9004965440068d36dbd150d19e4
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-19 21:18:00 +0100

    display questions and answers from props

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -3,14 +3,19 @@ import styles from './ListItem.module.scss';
 
 import arrow from '../../assets/images/icon-arrow-down.svg';
 
-const ListItem = () => {
+const ListItem = ({ question, answer }) => {
   return (
     <li className={styles.listItem}>
       <details className={styles.listItem__details}>
-        <summary className={styles.listItem__question}>How many members can I invite?
-        <div className={styles.listItem__arrow}><img src={arrow} alt="arrow" /></div>
+        <summary className={styles.listItem__question}>
+          {question}
+          <div className={styles.listItem__arrow}>
+            <img src={arrow} alt="arrow" />
+          </div>
         </summary>
-        <p className={styles.listItem__answer}>As much as you want</p>
+        <p className={styles.listItem__answer}>
+          {answer}
+        </p>
       </details>
     </li>
   )
