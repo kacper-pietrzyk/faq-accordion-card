f6c086036735c2d97777ec66bfb2970ab7507737
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-24 19:40:10 +0100

    update structure of component

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -3,22 +3,20 @@ import styles from './ListItem.module.scss';
 
 import arrow from '../../assets/images/icon-arrow-down.svg';
 
-const ListItem = ({ question, answer }) => {
-  return (
-    <li className={styles.listItem}>
-      <details className={styles.listItem__details}>
-        <summary className={styles.listItem__question}>
-          {question}
-          <div className={styles.listItem__arrow}>
-            <img src={arrow} alt="arrow" />
-          </div>
-        </summary>
-        <p className={styles.listItem__answer}>
-          {answer}
-        </p>
-      </details>
-    </li>
-  )
-};
+const ListItem = ({ question, answer }) => (
+  <li className={styles.listItem}>
+    <details className={styles.listItem__details}>
+      <summary className={styles.listItem__question}>
+        {question}
+        <div className={styles.listItem__arrow}>
+          <img src={arrow} alt="arrow" />
+        </div>
+      </summary>
+      <p className={styles.listItem__answer}>
+        {answer}
+      </p>
+    </details>
+  </li>
+);
 
 export default ListItem;
