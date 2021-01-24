0513f3c702f71afd952b81085caacfa2978fd9e9
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-24 17:11:26 +0100

    size of images depends on the width of the window

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,24 +2,42 @@ import React from 'react';
 import styles from './App.module.scss';
 
 import List from '../List/List';
+import useWindowWidth from './useWindowWidth';
 
-import image from '../../assets/images/illustration-woman-online-mobile.svg';
-import shadowOfImage from '../../assets/images/bg-pattern-mobile.svg';
+import mobileImage from '../../assets/images/illustration-woman-online-mobile.svg';
+import desktopImage from '../../assets/images/illustration-woman-online-desktop.svg';
+import desktopBoxImage from '../../assets/images/illustration-box-desktop.svg';
+import mobileShadowOfImage from '../../assets/images/bg-pattern-mobile.svg';
+import desktopShadowOfImage from '../../assets/images/bg-pattern-desktop.svg';
 
 const App = () => {
 
+  const windowWidth = useWindowWidth();
+  const imageSrc = windowWidth < 1024 ? mobileImage : desktopImage;
+  const shadowOfImageSrc = windowWidth < 1024 ? mobileShadowOfImage : desktopShadowOfImage;
+
   return (
-    <>
-      <div className={styles.wrapper}>
-        <div className={styles.images}>
-          <img className={styles.images__mainImage} src={image} alt="online technologies" />
-          <img className={styles.images__shadowImage} src={shadowOfImage} alt="shadow" />
-        </div>
+    <div className={styles.wrapper}>
+      <div className={styles.images}>
+        <img
+          className={styles.images__mainImage}
+          src={imageSrc}
+          alt="online technologies" />
+        <img
+          className={styles.images__shadowImage}
+          src={shadowOfImageSrc}
+          alt="shadow" />
+        {windowWidth >= 1024 &&
+          <img
+            className={styles.images__boxImage}
+            src={desktopBoxImage}
+            alt="box" />}
+      </div>
+      <main>
         <h1 className={styles.headline}>FAQ</h1>
         <List />
-      </div>
-    </>
+      </main>
+    </div>
   )
 };
-
 export default App;
