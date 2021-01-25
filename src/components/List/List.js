d898852e097d95d1a043c3eea3ac14bb4ed0153b
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-25 20:24:57 +0100

    update link to fetch data

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -8,7 +8,7 @@ const List = () => {
   const [faq, setFaq] = useState([]);
 
   useEffect(() => {
-    const url = "https://www.json-generator.com/api/json/get/bVpOOFOEky?indent=2";
+    const url = "https://next.json-generator.com/api/json/get/N16qFbOJ5";
     fetch(url)
       .then(response => response.json())
       .then(result => setFaq(result.faq))
