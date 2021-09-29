d34f0bc25eafc0336171d4dfe6a12f7f6e696078
Kacper Pietrzyk <kacper1609@gmail.com>
2021-09-29 23:39:39 +0200

    update fetch data

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -8,9 +8,16 @@ const List = () => {
   const [faq, setFaq] = useState([]);
 
   useEffect(() => {
-    const url = "https://glacial-anchorage-04547.herokuapp.com/https://api.json-generator.com/templates/W5spbkRAVOIp/data?access_token=i9i2ump0kzrk14xfg6h7gwvxseudmbpscyfeh6qh";
-    // const url = "https://api.json-generator.com/templates/W5spbkRAVOIp/data";
-    fetch(url)
+    // const url = "https://glacial-anchorage-04547.herokuapp.com/https://api.json-generator.com/templates/W5spbkRAVOIp/data?access_token=i9i2ump0kzrk14xfg6h7gwvxseudmbpscyfeh6qh";
+    const url = "https://glacial-anchorage-04547.herokuapp.com/https://api.json-generator.com/templates/W5spbkRAVOIp/data";
+    fetch(url, {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: "Bearer i9i2ump0kzrk14xfg6h7gwvxseudmbpscyfeh6qh",
+        requireHeader: ['origin', 'x-requested-with'],
+      }
+    })
       .then(response => response.json())
       .then(result => setFaq(result.faq))
       .catch(err => console.log(`błąd: ${err}`))
