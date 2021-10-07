1d1c5ccbad38c7191641dd918eaed2e1a57bd491
Kacper Pietrzyk <57292889+kacper-pietrzyk@users.noreply.github.com>
2021-10-07 07:38:55 +0200

    API url update

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -8,16 +8,8 @@ const List = () => {
   const [faq, setFaq] = useState([]);
 
   useEffect(() => {
-    // const url = "https://glacial-anchorage-04547.herokuapp.com/https://api.json-generator.com/templates/W5spbkRAVOIp/data?access_token=i9i2ump0kzrk14xfg6h7gwvxseudmbpscyfeh6qh";
-    const url = "https://glacial-anchorage-04547.herokuapp.com/https://api.json-generator.com/templates/W5spbkRAVOIp/data";
-    fetch(url, {
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: "Bearer i9i2ump0kzrk14xfg6h7gwvxseudmbpscyfeh6qh",
-        requireHeader: ['origin', 'x-requested-with'],
-      }
-    })
+    const url = "https://next.json-generator.com/api/json/get/N16qFbOJ5";
+    fetch(url)
       .then(response => response.json())
       .then(result => setFaq(result.faq))
       .catch(err => console.log(`błąd: ${err}`))
